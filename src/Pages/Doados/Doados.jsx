04c03a8dd58891ea12b5f57cefc25e8bd13b6256
AdrianaNasc <dryscaz@gmail.com>
2024-11-29 10:48:05 -0300

    atualização e criação do responsivo

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,9 +1,21 @@
+import S from './doados.module.scss'
+import capaum from '../../assets/img/capa-1.png'
 
 
 export default function Doados(){
     return(
-        <section>
-            <h2>Doados</h2>
+        <section className={S.container}>
+            <div>
+            <h2>Livros Doados</h2>
+            </div>
+            <section className={S.boxDoados}>
+                <article>
+                    <img src={capaum} alt="Capa do livro O protagonista nas cores vermelho e branco" />
+                    <h3>O protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p> 
+                </article>
+            </section>
         </section>
     )
 }
