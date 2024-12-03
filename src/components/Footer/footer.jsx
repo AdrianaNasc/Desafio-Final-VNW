b5aa68fd4b9237e31ecd0b15f48a5e154fd33a03
AdrianaNasc <dryscaz@gmail.com>
2024-12-03 19:09:00 -0300

    atualização

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,8 +1,8 @@
 
 import facebook from '../../assets/img/facebook-app.png'
 import instagram from '../../assets/img/instagram-app.png'
+import twitter from '../../assets/img/xsocial.png'
 import linkedin from '../../assets/img/linkedin-app.png'
-import twitter from '../../assets/img/twitter-app.png'
 import youtube from '../../assets/img/youtube-app.png'
 import S from './footer.module.scss'
 
@@ -13,8 +13,8 @@ export default function footer(){
                 <h3>4002-8922</h3>
                 <nav>
                     <a href=""><img src={facebook} alt="Logo da rede social Facebook" /></a>
-                    <a href=""><img src={twitter} alt="Logo da rede social antigo Twitter" /></a>
                     <a href=""><img src={youtube} alt="Logo da rede social Youtube" /></a>
+                    <a href=""><img src={twitter} alt="Logo da rede social antigo Twitter" /></a>
                     <a href=""><img src={linkedin} alt="Logo da rede social LinkedIn" /></a>
                     <a href=""><img src={instagram} alt="Logo da rede social Instagram" /></a>
                 </nav>
